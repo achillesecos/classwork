d2a4b222216c6898660d00df84b7738cedd1a9da
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-10-16 14:19:28 -0400

    ddd

---
+++ classwork-repository/src/arrays/ArraysMain.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class ArraysMain {
+	public ArraysMain() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated mehod stub
+	}
+	
+	public int diceRoll(int n) {
+		int output = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int randNum = (int)(Math.random() * 6);
+			result += randNum;
+		}
+	}
+
+}
