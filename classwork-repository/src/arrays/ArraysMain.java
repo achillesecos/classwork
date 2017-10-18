201c6bc7de319aca2e8a5574cda7f986a183c040
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-10-18 15:33:08 -0400

    New notes 10/18

--- classwork-repository/src/arrays/ArraysMain.java
+++ classwork-repository/src/arrays/ArraysMain.java
@@ -8,9 +8,50 @@ public class ArraysMain {
 	private String[] values; 
 	
 	public ArraysMain() {
-		tuesdayMethods();
+		wednesdayMethods();
 	}
 	
+	private void wednesdayMethods() {
+		int[] diceRolls = new int[10000];
+		populate(diceRolls);
+		int[] data = logestConsecutiveSeqAndIndex(diceRolls);
+		int longest = data[0];
+		System.out.println("The longest sequence is " + longest + " rolls."
+				+ " It happened on roll #" + data[1] + " the sequence was: "
+				+ Arrays.toString(subArray(diceRolls, data[1], data[0]))+ ".");
+	}
+	
+	/*
+	 * BIG IDEA:
+	 * Usually a method returns ONE piece of data (i.e. 'int', 'boolean', etc)
+	 * IF we ever want more than one piece of data, one way of doing that
+	 * is by using an array, as you see here, a method that returns the LENGTH 
+	 * of the sequence and its START position (both ints)
+	 */
+
+	private int[] logestConsecutiveSeqAndIndex(int[] arr) {
+		//use an int[] to store the data
+		int[] data = new int[2];//element at zero is length, at 1 is position 
+		
+		data[0] = 1;
+		int currentCount = 1;
+		for(int i = 0; i < arr.length; i++) {			
+			while(i + currentCount < arr.length && isConsecutive(arr, i, i + currentCount)) {
+				currentCount ++;
+			}
+			if(currentCount > maxLength) {
+				data[0] = currentCount;
+				//also store the index where this sequence started
+				data[1] = i;
+			}
+			i = i + currentCount -1; //saves time
+			currentCount = 1;
+		}
+		return maxLength;
+		
+		return null;
+	}
+
 	private void tuesdayMethods() {
 		int[] orderTest = {1,2,3,4,5,6,7,8,9,10};
 		cycleThrough(orderTest, 5);
