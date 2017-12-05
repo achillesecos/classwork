2c0ac01606e6abaf3f0c87c8a8bdc107942863d7
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-12-05 15:34:48 -0500

    12/5

---
+++ classwork-repository/src/algorithms/Fibonacci.java
@@ -0,0 +1,19 @@
+package algorithms;
+
+public class Fibonacci {
+
+	
+	public static void main(String[] args) {
+		System.out.println(fibonacci(8));
+	}
+	
+	public static int fibonacci(int n) {
+		if(n <= 2) {
+			return 1;
+		}
+		else {
+			return fibonacci(n-1) + fibonacci(n-2);
+		}
+	}
+	
+}
